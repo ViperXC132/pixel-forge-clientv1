cf816c1bb3e55e899ae6862d178b3b9916a12b92
Viper Davis <viperxc2204@gmail.com>
2026-09-02 14:32:45 +0100

    scaffold: add src/main/java/com/pixelforge/module/modules/visual/CrosshairModule.java

---
+++ src/main/java/com/pixelforge/module/modules/visual/CrosshairModule.java
@@ -0,0 +1,10 @@
+package com.pixelforge.module.modules.visual;
+
+import com.pixelforge.module.Category;
+import com.pixelforge.module.Module;
+
+public class CrosshairModule extends Module {
+    public CrosshairModule() { super("Crosshair", Category.VISUAL); }
+    @Override public void onEnable() {}
+    @Override public void onDisable() {}
+}
