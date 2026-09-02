e47887503dd995fd5b2e2074ede7cfecd949e4f9
Viper Davis <viperxc2204@gmail.com>
2026-09-02 14:32:59 +0100

    scaffold: add src/main/java/com/pixelforge/module/modules/utility/PotionAlertModule.java

---
+++ src/main/java/com/pixelforge/module/modules/utility/PotionAlertModule.java
@@ -0,0 +1,10 @@
+package com.pixelforge.module.modules.utility;
+
+import com.pixelforge.module.Category;
+import com.pixelforge.module.Module;
+
+public class PotionAlertModule extends Module {
+    public PotionAlertModule() { super("Potion Alert", Category.UTILITY); }
+    @Override public void onEnable() {}
+    @Override public void onDisable() {}
+}
