3185e710a64f13c7f22104a1dee0233b975a579d
Viper Davis <viperxc2204@gmail.com>
2026-09-02 14:32:48 +0100

    scaffold: add src/main/java/com/pixelforge/module/modules/visual/ArmorDurabilityModule.java

---
+++ src/main/java/com/pixelforge/module/modules/visual/ArmorDurabilityModule.java
@@ -0,0 +1,10 @@
+package com.pixelforge.module.modules.visual;
+
+import com.pixelforge.module.Category;
+import com.pixelforge.module.Module;
+
+public class ArmorDurabilityModule extends Module {
+    public ArmorDurabilityModule() { super("Armor Durability", Category.VISUAL); }
+    @Override public void onEnable() {}
+    @Override public void onDisable() {}
+}
