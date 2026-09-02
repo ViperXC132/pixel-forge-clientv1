1e8dcbd1d85680ac7dfaabb5254981b62e14c1b6
Viper Davis <viperxc2204@gmail.com>
2026-09-02 14:32:50 +0100

    scaffold: add src/main/java/com/pixelforge/module/modules/utility/FreeLookModule.java

---
+++ src/main/java/com/pixelforge/module/modules/utility/FreeLookModule.java
@@ -0,0 +1,10 @@
+package com.pixelforge.module.modules.utility;
+
+import com.pixelforge.module.Category;
+import com.pixelforge.module.Module;
+
+public class FreeLookModule extends Module {
+    public FreeLookModule() { super("Free Look", Category.UTILITY); }
+    @Override public void onEnable() {}
+    @Override public void onDisable() {}
+}
