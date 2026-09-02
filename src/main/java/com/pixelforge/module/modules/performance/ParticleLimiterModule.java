00675a07973ee5b682213f04c1bc8a76bf29e981
Viper Davis <viperxc2204@gmail.com>
2026-09-02 14:33:07 +0100

    scaffold: add src/main/java/com/pixelforge/module/modules/performance/ParticleLimiterModule.java

---
+++ src/main/java/com/pixelforge/module/modules/performance/ParticleLimiterModule.java
@@ -0,0 +1,10 @@
+package com.pixelforge.module.modules.performance;
+
+import com.pixelforge.module.Category;
+import com.pixelforge.module.Module;
+
+public class ParticleLimiterModule extends Module {
+    public ParticleLimiterModule() { super("Particle Limiter", Category.PERFORMANCE); }
+    @Override public void onEnable() {}
+    @Override public void onDisable() {}
+}
