7c70afff2ebdfb4751ce1324e5a741915ea3dd1a
Viper Davis <viperxc2204@gmail.com>
2026-09-02 14:33:09 +0100

    scaffold: add src/main/java/com/pixelforge/module/modules/movement/ToggleSprintModule.java

---
+++ src/main/java/com/pixelforge/module/modules/movement/ToggleSprintModule.java
@@ -0,0 +1,10 @@
+package com.pixelforge.module.modules.movement;
+
+import com.pixelforge.module.Category;
+import com.pixelforge.module.Module;
+
+public class ToggleSprintModule extends Module {
+    public ToggleSprintModule() { super("Toggle Sprint", Category.MOVEMENT); }
+    @Override public void onEnable() {}
+    @Override public void onDisable() {}
+}
