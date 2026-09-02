85c4151dd2a3e1400dd87815d4f791902eb8691c
Viper Davis <viperxc2204@gmail.com>
2026-09-02 14:32:54 +0100

    scaffold: add src/main/java/com/pixelforge/module/modules/utility/BetterChatModule.java

---
+++ src/main/java/com/pixelforge/module/modules/utility/BetterChatModule.java
@@ -0,0 +1,10 @@
+package com.pixelforge.module.modules.utility;
+
+import com.pixelforge.module.Category;
+import com.pixelforge.module.Module;
+
+public class BetterChatModule extends Module {
+    public BetterChatModule() { super("Better Chat", Category.UTILITY); }
+    @Override public void onEnable() {}
+    @Override public void onDisable() {}
+}
