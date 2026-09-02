0bb0d5ac1d914f7566bd61474e9a594e16b06c71
Viper Davis <viperxc2204@gmail.com>
2026-09-02 14:32:43 +0100

    scaffold: add src/main/java/com/pixelforge/module/modules/visual/HitColorModule.java

---
+++ src/main/java/com/pixelforge/module/modules/visual/HitColorModule.java
@@ -0,0 +1,10 @@
+package com.pixelforge.module.modules.visual;
+
+import com.pixelforge.module.Category;
+import com.pixelforge.module.Module;
+
+public class HitColorModule extends Module {
+    public HitColorModule() { super("Hit Color", Category.VISUAL); }
+    @Override public void onEnable() {}
+    @Override public void onDisable() {}
+}
