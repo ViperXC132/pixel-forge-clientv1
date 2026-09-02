2c9f683a97aa978f84ed853efb318e4d079fc3fb
Viper Davis <viperxc2204@gmail.com>
2026-09-02 14:32:55 +0100

    scaffold: add src/main/java/com/pixelforge/module/modules/utility/FastPlaceModule.java

---
+++ src/main/java/com/pixelforge/module/modules/utility/FastPlaceModule.java
@@ -0,0 +1,10 @@
+package com.pixelforge.module.modules.utility;
+
+import com.pixelforge.module.Category;
+import com.pixelforge.module.Module;
+
+public class FastPlaceModule extends Module {
+    public FastPlaceModule() { super("Fast Place", Category.UTILITY); }
+    @Override public void onEnable() {}
+    @Override public void onDisable() {}
+}
