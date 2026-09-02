93bc346b5db5eebbd57807bd64cf2ca37e0f72bd
Viper Davis <viperxc2204@gmail.com>
2026-09-02 14:33:04 +0100

    scaffold: add src/main/java/com/pixelforge/module/modules/utility/FovChangerModule.java

---
+++ src/main/java/com/pixelforge/module/modules/utility/FovChangerModule.java
@@ -0,0 +1,10 @@
+package com.pixelforge.module.modules.utility;
+
+import com.pixelforge.module.Category;
+import com.pixelforge.module.Module;
+
+public class FovChangerModule extends Module {
+    public FovChangerModule() { super("FOV Changer", Category.UTILITY); }
+    @Override public void onEnable() {}
+    @Override public void onDisable() {}
+}
