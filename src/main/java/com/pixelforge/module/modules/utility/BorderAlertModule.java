f5bd3a1dc9bd64fac79060ca40be1eaa4a6eee2f
Viper Davis <viperxc2204@gmail.com>
2026-09-02 14:33:00 +0100

    scaffold: add src/main/java/com/pixelforge/module/modules/utility/BorderAlertModule.java

---
+++ src/main/java/com/pixelforge/module/modules/utility/BorderAlertModule.java
@@ -0,0 +1,10 @@
+package com.pixelforge.module.modules.utility;
+
+import com.pixelforge.module.Category;
+import com.pixelforge.module.Module;
+
+public class BorderAlertModule extends Module {
+    public BorderAlertModule() { super("Border Alert", Category.UTILITY); }
+    @Override public void onEnable() {}
+    @Override public void onDisable() {}
+}
