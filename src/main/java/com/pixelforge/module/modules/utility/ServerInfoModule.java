e558be316e0bc3f742ca35e55f2ea88d0a40ac30
Viper Davis <viperxc2204@gmail.com>
2026-09-02 14:33:01 +0100

    scaffold: add src/main/java/com/pixelforge/module/modules/utility/ServerInfoModule.java

---
+++ src/main/java/com/pixelforge/module/modules/utility/ServerInfoModule.java
@@ -0,0 +1,10 @@
+package com.pixelforge.module.modules.utility;
+
+import com.pixelforge.module.Category;
+import com.pixelforge.module.Module;
+
+public class ServerInfoModule extends Module {
+    public ServerInfoModule() { super("Server Info", Category.UTILITY); }
+    @Override public void onEnable() {}
+    @Override public void onDisable() {}
+}
