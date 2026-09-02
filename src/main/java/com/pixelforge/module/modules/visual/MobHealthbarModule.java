a2c787c7baeeb02bbb2f10000a0e0ce8cbce8008
Viper Davis <viperxc2204@gmail.com>
2026-09-02 14:32:46 +0100

    scaffold: add src/main/java/com/pixelforge/module/modules/visual/MobHealthbarModule.java

---
+++ src/main/java/com/pixelforge/module/modules/visual/MobHealthbarModule.java
@@ -0,0 +1,10 @@
+package com.pixelforge.module.modules.visual;
+
+import com.pixelforge.module.Category;
+import com.pixelforge.module.Module;
+
+public class MobHealthbarModule extends Module {
+    public MobHealthbarModule() { super("Mob Healthbar", Category.VISUAL); }
+    @Override public void onEnable() {}
+    @Override public void onDisable() {}
+}
