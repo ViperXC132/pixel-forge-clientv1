cba2be448433f245db2223e4ccb03da63588ae9f
Viper Davis <viperxc2204@gmail.com>
2026-09-02 14:32:51 +0100

    scaffold: add src/main/java/com/pixelforge/module/modules/utility/AutoRespawnModule.java

---
+++ src/main/java/com/pixelforge/module/modules/utility/AutoRespawnModule.java
@@ -0,0 +1,10 @@
+package com.pixelforge.module.modules.utility;
+
+import com.pixelforge.module.Category;
+import com.pixelforge.module.Module;
+
+public class AutoRespawnModule extends Module {
+    public AutoRespawnModule() { super("Auto Respawn", Category.UTILITY); }
+    @Override public void onEnable() {}
+    @Override public void onDisable() {}
+}
