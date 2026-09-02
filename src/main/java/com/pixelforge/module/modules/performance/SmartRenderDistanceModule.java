558fe9f6b45c2ec1c010846e30435e7f8b1a7c83
Viper Davis <viperxc2204@gmail.com>
2026-09-02 14:33:06 +0100

    scaffold: add src/main/java/com/pixelforge/module/modules/performance/SmartRenderDistanceModule.java

---
+++ src/main/java/com/pixelforge/module/modules/performance/SmartRenderDistanceModule.java
@@ -0,0 +1,10 @@
+package com.pixelforge.module.modules.performance;
+
+import com.pixelforge.module.Category;
+import com.pixelforge.module.Module;
+
+public class SmartRenderDistanceModule extends Module {
+    public SmartRenderDistanceModule() { super("Smart Render Distance", Category.PERFORMANCE); }
+    @Override public void onEnable() {}
+    @Override public void onDisable() {}
+}
