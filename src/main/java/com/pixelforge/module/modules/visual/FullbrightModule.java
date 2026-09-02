bbffe4d92e9aa194e249eeedc67c086999fb5078
Viper Davis <viperxc2204@gmail.com>
2026-09-02 14:32:41 +0100

    scaffold: add src/main/java/com/pixelforge/module/modules/visual/FullbrightModule.java

---
+++ src/main/java/com/pixelforge/module/modules/visual/FullbrightModule.java
@@ -0,0 +1,10 @@
+package com.pixelforge.module.modules.visual;
+
+import com.pixelforge.module.Category;
+import com.pixelforge.module.Module;
+
+public class FullbrightModule extends Module {
+    public FullbrightModule() { super("Fullbright", Category.VISUAL); }
+    @Override public void onEnable() {}
+    @Override public void onDisable() {}
+}
