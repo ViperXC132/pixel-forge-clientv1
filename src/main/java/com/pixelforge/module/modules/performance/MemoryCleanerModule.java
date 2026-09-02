8433a8dd2aa762dd28ecf4114e69ce7af3cecfdc
Viper Davis <viperxc2204@gmail.com>
2026-09-02 14:33:08 +0100

    scaffold: add src/main/java/com/pixelforge/module/modules/performance/MemoryCleanerModule.java

---
+++ src/main/java/com/pixelforge/module/modules/performance/MemoryCleanerModule.java
@@ -0,0 +1,10 @@
+package com.pixelforge.module.modules.performance;
+
+import com.pixelforge.module.Category;
+import com.pixelforge.module.Module;
+
+public class MemoryCleanerModule extends Module {
+    public MemoryCleanerModule() { super("Memory Cleaner", Category.PERFORMANCE); }
+    @Override public void onEnable() {}
+    @Override public void onDisable() {}
+}
