defca0d7bacaac405bcc111fdfbcdf132f2ee1a3
Viper Davis <viperxc2204@gmail.com>
2026-09-02 14:32:53 +0100

    scaffold: add src/main/java/com/pixelforge/module/modules/utility/NoHurtCamModule.java

---
+++ src/main/java/com/pixelforge/module/modules/utility/NoHurtCamModule.java
@@ -0,0 +1,10 @@
+package com.pixelforge.module.modules.utility;
+
+import com.pixelforge.module.Category;
+import com.pixelforge.module.Module;
+
+public class NoHurtCamModule extends Module {
+    public NoHurtCamModule() { super("No Hurt Cam", Category.UTILITY); }
+    @Override public void onEnable() {}
+    @Override public void onDisable() {}
+}
