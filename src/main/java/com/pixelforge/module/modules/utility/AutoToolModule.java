80762392c8b304b43bf11fa135bb570ee36c2f2d
Viper Davis <viperxc2204@gmail.com>
2026-09-02 14:32:57 +0100

    scaffold: add src/main/java/com/pixelforge/module/modules/utility/AutoToolModule.java

---
+++ src/main/java/com/pixelforge/module/modules/utility/AutoToolModule.java
@@ -0,0 +1,10 @@
+package com.pixelforge.module.modules.utility;
+
+import com.pixelforge.module.Category;
+import com.pixelforge.module.Module;
+
+public class AutoToolModule extends Module {
+    public AutoToolModule() { super("Auto Tool", Category.UTILITY); }
+    @Override public void onEnable() {}
+    @Override public void onDisable() {}
+}
