68efb068f3b7947362412ab8b103c50ec52251e3
Viper Davis <viperxc2204@gmail.com>
2026-09-02 14:33:03 +0100

    scaffold: add src/main/java/com/pixelforge/module/modules/utility/ScreenshotManagerModule.java

---
+++ src/main/java/com/pixelforge/module/modules/utility/ScreenshotManagerModule.java
@@ -0,0 +1,10 @@
+package com.pixelforge.module.modules.utility;
+
+import com.pixelforge.module.Category;
+import com.pixelforge.module.Module;
+
+public class ScreenshotManagerModule extends Module {
+    public ScreenshotManagerModule() { super("Screenshot Manager", Category.UTILITY); }
+    @Override public void onEnable() {}
+    @Override public void onDisable() {}
+}
