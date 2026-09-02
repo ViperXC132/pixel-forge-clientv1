c99fd567d3bed4b17c5c6b520a28d4abb5e3f659
Viper Davis <viperxc2204@gmail.com>
2026-09-02 14:32:42 +0100

    scaffold: add src/main/java/com/pixelforge/module/modules/visual/CustomNametags.java

---
+++ src/main/java/com/pixelforge/module/modules/visual/CustomNametags.java
@@ -0,0 +1,10 @@
+package com.pixelforge.module.modules.visual;
+
+import com.pixelforge.module.Category;
+import com.pixelforge.module.Module;
+
+public class CustomNametags extends Module {
+    public CustomNametags() { super("Custom Nametags", Category.VISUAL); }
+    @Override public void onEnable() {}
+    @Override public void onDisable() {}
+}
