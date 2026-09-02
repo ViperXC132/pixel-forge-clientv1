855fd643e18b5390b17c7368a2a18222d8ef5907
Viper Davis <viperxc2204@gmail.com>
2026-09-02 14:33:06 +0100

    scaffold: add src/main/java/com/pixelforge/module/modules/performance/EntityCullingModule.java

---
+++ src/main/java/com/pixelforge/module/modules/performance/EntityCullingModule.java
@@ -0,0 +1,10 @@
+package com.pixelforge.module.modules.performance;
+
+import com.pixelforge.module.Category;
+import com.pixelforge.module.Module;
+
+public class EntityCullingModule extends Module {
+    public EntityCullingModule() { super("Entity Culling", Category.PERFORMANCE); }
+    @Override public void onEnable() {}
+    @Override public void onDisable() {}
+}
