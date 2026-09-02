a2fd812017067028272d7f5401db52240942f6e4
Viper Davis <viperxc2204@gmail.com>
2026-09-02 14:32:47 +0100

    scaffold: add src/main/java/com/pixelforge/module/modules/visual/BlockOutlineModule.java

---
+++ src/main/java/com/pixelforge/module/modules/visual/BlockOutlineModule.java
@@ -0,0 +1,10 @@
+package com.pixelforge.module.modules.visual;
+
+import com.pixelforge.module.Category;
+import com.pixelforge.module.Module;
+
+public class BlockOutlineModule extends Module {
+    public BlockOutlineModule() { super("Block Outline", Category.VISUAL); }
+    @Override public void onEnable() {}
+    @Override public void onDisable() {}
+}
