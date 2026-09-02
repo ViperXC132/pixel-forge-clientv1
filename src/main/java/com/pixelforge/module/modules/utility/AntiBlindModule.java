8c5f2eb9f3f2b6223cf9003cc2f275f6c9ed50ac
Viper Davis <viperxc2204@gmail.com>
2026-09-02 14:32:52 +0100

    scaffold: add src/main/java/com/pixelforge/module/modules/utility/AntiBlindModule.java

---
+++ src/main/java/com/pixelforge/module/modules/utility/AntiBlindModule.java
@@ -0,0 +1,10 @@
+package com.pixelforge.module.modules.utility;
+
+import com.pixelforge.module.Category;
+import com.pixelforge.module.Module;
+
+public class AntiBlindModule extends Module {
+    public AntiBlindModule() { super("Anti Blind", Category.UTILITY); }
+    @Override public void onEnable() {}
+    @Override public void onDisable() {}
+}
