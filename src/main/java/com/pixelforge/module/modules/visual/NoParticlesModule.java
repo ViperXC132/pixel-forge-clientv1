c040e613d9a980b0bcadcb0cb08153c1437fce3d
Viper Davis <viperxc2204@gmail.com>
2026-09-02 14:32:49 +0100

    scaffold: add src/main/java/com/pixelforge/module/modules/visual/NoParticlesModule.java

---
+++ src/main/java/com/pixelforge/module/modules/visual/NoParticlesModule.java
@@ -0,0 +1,10 @@
+package com.pixelforge.module.modules.visual;
+
+import com.pixelforge.module.Category;
+import com.pixelforge.module.Module;
+
+public class NoParticlesModule extends Module {
+    public NoParticlesModule() { super("No Particles", Category.VISUAL); }
+    @Override public void onEnable() {}
+    @Override public void onDisable() {}
+}
