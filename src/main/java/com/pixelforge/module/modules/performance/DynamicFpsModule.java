761ca5bf5c873f4c59fa15dd49683bf0c71f115c
Viper Davis <viperxc2204@gmail.com>
2026-09-02 14:33:05 +0100

    scaffold: add src/main/java/com/pixelforge/module/modules/performance/DynamicFpsModule.java

---
+++ src/main/java/com/pixelforge/module/modules/performance/DynamicFpsModule.java
@@ -0,0 +1,10 @@
+package com.pixelforge.module.modules.performance;
+
+import com.pixelforge.module.Category;
+import com.pixelforge.module.Module;
+
+public class DynamicFpsModule extends Module {
+    public DynamicFpsModule() { super("Dynamic FPS", Category.PERFORMANCE); }
+    @Override public void onEnable() {}
+    @Override public void onDisable() {}
+}
