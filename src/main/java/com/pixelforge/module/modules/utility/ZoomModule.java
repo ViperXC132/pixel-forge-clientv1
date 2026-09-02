7518e43c2af636ae59098f84cfbb33fbbb39f9de
Viper Davis <viperxc2204@gmail.com>
2026-09-02 14:32:49 +0100

    scaffold: add src/main/java/com/pixelforge/module/modules/utility/ZoomModule.java

---
+++ src/main/java/com/pixelforge/module/modules/utility/ZoomModule.java
@@ -0,0 +1,10 @@
+package com.pixelforge.module.modules.utility;
+
+import com.pixelforge.module.Category;
+import com.pixelforge.module.Module;
+
+public class ZoomModule extends Module {
+    public ZoomModule() { super("Zoom", Category.UTILITY); }
+    @Override public void onEnable() {}
+    @Override public void onDisable() {}
+}
