cba42ee33853b71bf1db52338b52a92a7a236209
Viper Davis <viperxc2204@gmail.com>
2026-09-02 14:33:10 +0100

    scaffold: add src/main/java/com/pixelforge/module/modules/movement/ToggleSneakModule.java

---
+++ src/main/java/com/pixelforge/module/modules/movement/ToggleSneakModule.java
@@ -0,0 +1,10 @@
+package com.pixelforge.module.modules.movement;
+
+import com.pixelforge.module.Category;
+import com.pixelforge.module.Module;
+
+public class ToggleSneakModule extends Module {
+    public ToggleSneakModule() { super("Toggle Sneak", Category.MOVEMENT); }
+    @Override public void onEnable() {}
+    @Override public void onDisable() {}
+}
