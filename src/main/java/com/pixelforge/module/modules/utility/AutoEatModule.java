48a8d369ff7da3ad5f0adf715d7728c599407c77
Viper Davis <viperxc2204@gmail.com>
2026-09-02 14:32:58 +0100

    scaffold: add src/main/java/com/pixelforge/module/modules/utility/AutoEatModule.java

---
+++ src/main/java/com/pixelforge/module/modules/utility/AutoEatModule.java
@@ -0,0 +1,10 @@
+package com.pixelforge.module.modules.utility;
+
+import com.pixelforge.module.Category;
+import com.pixelforge.module.Module;
+
+public class AutoEatModule extends Module {
+    public AutoEatModule() { super("Auto Eat", Category.UTILITY); }
+    @Override public void onEnable() {}
+    @Override public void onDisable() {}
+}
