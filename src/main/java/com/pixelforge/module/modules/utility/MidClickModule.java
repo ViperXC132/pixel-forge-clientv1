8ca427ae529cb76e08c917ddf21190aabebdd273
Viper Davis <viperxc2204@gmail.com>
2026-09-02 14:32:56 +0100

    scaffold: add src/main/java/com/pixelforge/module/modules/utility/MidClickModule.java

---
+++ src/main/java/com/pixelforge/module/modules/utility/MidClickModule.java
@@ -0,0 +1,10 @@
+package com.pixelforge.module.modules.utility;
+
+import com.pixelforge.module.Category;
+import com.pixelforge.module.Module;
+
+public class MidClickModule extends Module {
+    public MidClickModule() { super("Mid Click", Category.UTILITY); }
+    @Override public void onEnable() {}
+    @Override public void onDisable() {}
+}
